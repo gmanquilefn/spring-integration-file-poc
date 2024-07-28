d176c130d5e959696cb6e8fbe7a5a2d3e6ed5817
Gonzalo Manquilef <g.manquilef.n@gmail.com>
2024-07-28 17:12:45 -0400

    initial commit

---
+++ src/main/java/cl/gfmn/FilePipelineApplication.java
@@ -0,0 +1,13 @@
+package cl.gfmn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilePipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilePipelineApplication.class, args);
+	}
+
+}
